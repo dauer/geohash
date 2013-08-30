f0b06f9253793b0e7efcef102b6da643b7d64253
Thomas Rasmussen <thomas.bregentved@gmail.com>
2013-08-30 20:59:26 +0200

    Changed example to show console output when running code

--- lua/example.lua
+++ lua/example.lua
@@ -1,5 +1,15 @@
 GeoHash = require("lib/geohash")
 
-GeoHash.precision(8)
 print(GeoHash.decode("ub52mwcbpupf"))
+-- 45.081046   38.564758
+
 print(GeoHash.encode(45.081046, 38.564758))
+-- ub52mwcbpupf
+
+GeoHash.precision(4)
+
+print(GeoHash.decode("ub52mwcbpupf"))
+-- 45.081046   38.564758
+
+print(GeoHash.encode(45.081046, 38.564758))
+-- ub52mwcb
