733ce6c98ba00d31179fac35d5a6b59303a60e93
Thomas Rasmussen <thomas.bregentved@gmail.com>
2014-10-12 23:34:05 +0200

    Added output to example.php script

--- php/example.php
+++ php/example.php
@@ -6,10 +6,16 @@ use \dk\workbench\geohash\GeoHash as GeoHash;
 
 $hash = GeoHash::encode(45.081046, 38.564758);
 echo $hash . "\n";
+// ub52mwcbpupf
 
 $coordinates = GeoHash::decode('ub52mwcbpupf');
 echo 'Latitude: ' . $coordinates->latitude . "\n";
+// Latitude: 45.081046
+
 echo 'Longitude: ' . $coordinates->longitude . "\n";
+// Longitude: 38.564758
+
 echo 'Coordinate: ' . $coordinates . "\n";
+// Coordinate: 45.081046, 38.564758
 
 ?>
